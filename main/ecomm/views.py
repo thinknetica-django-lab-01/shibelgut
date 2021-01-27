7d6e1712c19ada73e215d5510823fdac35770e4a
Alexander Shibelgut <shibelgut@gmail.com>
2021-01-27 18:00:15 +0700

    Changed the definition of current user

--- main/ecomm/views.py
+++ main/ecomm/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 
 def index(request):
     turn_on_block = True
-    current_username = 'Mikhail'
+    current_username = request.user
     return render(request, 'ecomm/index.html', context={'turn_on_block': turn_on_block, 'current_username': current_username})
