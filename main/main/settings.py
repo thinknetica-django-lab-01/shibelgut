ae227e92bf10148996c14a28a07fe523df0b6faf
Alexander Shibelgut <shibelgut@gmail.com>
2021-02-20 20:08:50 +0700

    Installed Redis and configured the cache for Redis

--- main/main/settings.py
+++ main/main/settings.py
@@ -199,3 +199,14 @@ CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Asia/Tomsk'
 
+# Caches
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://127.0.0.1:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
+        }
+    }
+}
+
