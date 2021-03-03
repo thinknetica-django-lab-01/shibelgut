4d573c9276a95dff708cbb2748dd7a1e60001f76
Alexander Shibelgut <shibelgut@gmail.com>
2021-03-03 12:47:49 +0700

    Configured admin panel

--- main/ecomm/admin.py
+++ main/ecomm/admin.py
@@ -5,6 +5,8 @@ from django.contrib.flatpages.admin import FlatPageAdmin as FlatPageAdminOrigina
 from django.contrib.flatpages.admin import FlatpageForm as FlatpageFormOriginal
 from django.contrib.flatpages.models import FlatPage
 
+from ecomm.models import Characteristic, Category, CustomUser, Good, Image, Tag, Seller
+
 
 class FlatpageForm(FlatpageFormOriginal):
     content = forms.CharField(widget=CKEditorWidget())
@@ -18,5 +20,36 @@ class FlatPageAdmin(FlatPageAdminOriginal):
     form = FlatpageForm
 
 
+def make_published(modeladmin, request, queryset):
+    for obj in queryset:
+        if not obj.available:
+            obj.available = True
+        obj.save()
+
+
+make_published.short_description = 'Make selected goods as published'
+
+
+def make_saved_in_archive(modeladmin, request, queryset):
+    for obj in queryset:
+        if obj.available:
+            obj.available = False
+        obj.save()
+
+
+make_published.short_description = 'Make selected goods as saved in archive'
+
+
+class GoodAdmin(admin.ModelAdmin):
+    date_hierarchy = 'pub_date'
+    list_display = ('title', 'price', 'quantity', 'is_available', 'issue_date', 'tag')
+    list_display_links = ('title', 'tag')
+    list_editable = ('price', 'issue_date')
+    list_filter = ('tag', )
+    search_fields = ('title', )
+    actions = [make_published, make_saved_in_archive]
+
+
 admin.site.unregister(FlatPage)
 admin.site.register(FlatPage, FlatPageAdmin)
+admin.site.register(Good, GoodAdmin)
